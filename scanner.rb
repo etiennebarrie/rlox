5e1d2657c1d46f0a7927763b9296e167b01e4724
Étienne Barrié <etienne.barrie@gmail.com>
2025-04-25 14:32:37 +0200

    4.5 Recognizing Lexemes

--- scanner.rb
+++ scanner.rb
@@ -18,5 +18,30 @@ module Lox
   private
     def end? = @current >= @source.size
 
+    def scan_token
+      case c = advance
+      when "(" then add_token :LEFT_PAREN
+      when ")" then add_token :RIGHT_PAREN
+      when "{" then add_token :LEFT_BRACE
+      when "}" then add_token :RIGHT_BRACE
+      when "," then add_token :COMMA
+      when "." then add_token :DOT
+      when "-" then add_token :MINUS
+      when "+" then add_token :PLUS
+      when ";" then add_token :SEMICOLON
+      when "*" then add_token :STAR
+      end
+    end
+
+    def advance
+      @source[@current].tap do
+        @current += 1
+      end
+    end
+
+    def add_token type, literal = nil
+      text = @source[@start...@current]
+      Token.new type, text, literal, @line
+    end
   end
 end
